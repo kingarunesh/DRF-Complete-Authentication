fe51b64ebe1dbf5a9bf4c7f3306f85a2e71e9b8a
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-22 19:46:00 +0530

    3.1 : register new user

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from account.serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+class UserRegisterView(APIView):
+    def post(self, request, format=None):
+
+        #!  send data
+        serializer = UserRegistrationSerializer(data=request.data)
+
+        #!  check valid data
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "Register successfull"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
