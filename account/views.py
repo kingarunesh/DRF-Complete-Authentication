f963d0a171ab2ad0cbe0e6b42ca2aa32c722c40c
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-23 19:08:56 +0530

    10.1 : code refactor

--- account/views.py
+++ account/views.py
@@ -21,38 +21,34 @@ class UserRegisterView(APIView):
     renderer_classes = [UserRenderer]
 
     def post(self, request, format=None):
-
         #!  send data
         serializer = UserRegistrationSerializer(data=request.data)
 
         #!  check valid data
-        if serializer.is_valid(raise_exception=True):
-            user = serializer.save()
-            token = get_tokens_for_user(user)
-            return Response({"token": token, "message": "Register successfull"}, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response({"token": token, "message": "Register successfull"}, status=status.HTTP_201_CREATED)    
 
 
 #!  login
 class LoginView(APIView):
     def post(self, request, format=None):
         serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            email = serializer.data.get("email")
-            password = serializer.data.get("password")
 
-            user = authenticate(email=email, password=password)
+        serializer.is_valid(raise_exception=True)
+        
+        email = serializer.data.get("email")
+        password = serializer.data.get("password")
+        user = authenticate(email=email, password=password)
 
-            if user is not None:
-                token = get_tokens_for_user(user)
+        if user is not None:
+            token = get_tokens_for_user(user)
 
-                return Response({"token": token, "message": "Login Success"}, status=status.HTTP_200_OK)
-            else:
-                return Response({"error": {"non_field_errors": ["Email or Password not found"]}}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"token": token, "message": "Login Success"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": {"non_field_errors": ["Email or Password not found"]}},status=status.HTTP_404_NOT_FOUND)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -74,11 +70,9 @@ class ChangePasswordView(APIView):
     def post(self, request, format=None):
         serializer = ChangePasswordSerializer(data=request.data, context={"user": request.user})
 
-        if serializer.is_valid(raise_exception=True):
-            return Response({"message": "Password Changed"}, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
+        
+        return Response({"message": "Password Changed"}, status=status.HTTP_200_OK)
 
 
 #!  Send Password Reset
@@ -88,11 +82,8 @@ class SendPasswordResetView(APIView):
     def post(self, request, format=None):
         serializer = SendPasswordResetSerializer(data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            return Response({"message": "Reset Password Link Sent On Your Email, Please Check Your Email."}, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)
+        return Response({"message": "Reset Password Link Sent On Your Email, Please Check Your Email."}, status=status.HTTP_200_OK)
 
 
 #!  Reset Password
@@ -102,7 +93,5 @@ class PasswordResetView(APIView):
     def post(self, request, uid, token, format=None):
         serializer = PasswordResetSerializer(data=request.data, context={"uid": uid, "token": token})
 
-        if serializer.is_valid(raise_exception=True):
-            return Response({"message": "Password Reset Successfully."}, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        return Response({"message": "Password Reset Successfully."}, status=status.HTTP_200_OK)
