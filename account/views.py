be2079eec31413af37b6d91eb798c62b739fcc91
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-23 10:41:05 +0530

    6.1 : json jwt implement on view.py file

--- account/views.py
+++ account/views.py
@@ -4,6 +4,15 @@ from rest_framework import status
 from account.serializers import UserRegistrationSerializer, LoginSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 
 class UserRegisterView(APIView):
@@ -17,7 +26,8 @@ class UserRegisterView(APIView):
         #!  check valid data
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({"message": "Register successfull"}, status=status.HTTP_201_CREATED)
+            token = get_tokens_for_user(user)
+            return Response({"token": token, "message": "Register successfull"}, status=status.HTTP_201_CREATED)
         
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -33,7 +43,9 @@ class LoginView(APIView):
             user = authenticate(email=email, password=password)
 
             if user is not None:
-                return Response({"message": "Login Success"}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+
+                return Response({"token": token, "message": "Login Success"}, status=status.HTTP_200_OK)
             else:
                 return Response({"error": {"non_field_errors": ["Email or Password not found"]}}, status=status.HTTP_404_NOT_FOUND)
 
